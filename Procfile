284ec9d354bd46796ac794ad6742f0d056efe06d
martin <vazquezmartin1240@gmail.com>
2024-02-25 18:31:17 -0600

    whitenoise

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn plataforma.wsgi
+web: python manage.py collectstatic && gunicorn plataforma.wsgi
--- plataforma/settings.py
+++ plataforma/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'whitenoise.runserver_nostatic',
     'alumnos',
     'clases',
     'rest_framework.authtoken',
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -120,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -135,3 +138,5 @@ REST_FRAMEWORK = {
                 'rest_framework.authentication.TokenAuthentication',
             ]
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
