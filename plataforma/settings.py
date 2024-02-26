2853a247aadd4beba6c86a5ea629a059aff9c8b4
martin <vazquezmartin1240@gmail.com>
2024-02-25 18:51:45 -0600

    se arreglo el erorr

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-!g2=4q%*zq4vrqr-2y#rpu95=*9a$+xxx%l%hqd43^r=70^e+h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','web-production-a333.up.railway.app']
+ALLOWED_HOSTS = ['localhost','web-production-a333.up.railway.app','127.0.0.1']
 
 
 # Application definition
